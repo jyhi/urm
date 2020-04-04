ad5e167f1b192aba02ffc0ad78531d325005feab
Junde Yhi <lmy441900@aosc.xyz>
2020-04-04 23:16:00 +0800

    src/dashboard.rs: remove outdated comments

--- src/dashboard.rs
+++ src/dashboard.rs
@@ -17,10 +17,6 @@ pub fn ui(urm_info: State<UrmInfo>) -> Template {
   let repositories = Repositories { number: 0 };
   let products = Products { number: 0 };
 
-  // XXX: "Using this method is typically unnecessary as State implements Deref
-  // with a Deref::Target of T." Why inner() is required for rustc to satisfy
-  // the trait constraint of Template::render()?
-  // https://api.rocket.rs/v0.4/rocket/struct.State.html#method.inner
   let ctx = UrmContext {
     urm: &urm_info,
     repositories: &repositories,
