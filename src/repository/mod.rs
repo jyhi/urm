2158664fda2da3c1fe6d9ad6c1953a622ddc597a
Junde Yhi <lmy441900@aosc.xyz>
2020-04-12 14:46:04 +0800

    repository: add product list in current repository (back)

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -24,11 +24,14 @@ pub fn api(db: UrmDb, ln_p: String)
   }
 }
 
-#[get("/repository/<ln_p>", format = "html", rank = 1)]
-pub fn ui(config: State<UrmConfig>, db: UrmDb, ln_p: String)
+#[get("/repository/<ln_p>?<page>&<nitem>", format = "html", rank = 1)]
+pub fn ui(config: State<UrmConfig>, db: UrmDb, ln_p: String, page: Option<u64>, nitem: Option<u64>)
   -> Result<Option<Template>, mongodb::error::Error>
 {
-  match ui::Context::from_db(&db, &config, ln_p) {
+  let page = page.unwrap_or(1);
+  let nitem = nitem.unwrap_or(10);
+
+  match ui::Context::from_db(&db, &config, ln_p, page, nitem) {
     Ok(r) => match r {
       Some(ctx) => Ok(Some(Template::render("repository", ctx))),
       None => Ok(None)
