jyhi/urm

commit de92f6bb60011453cc8343f0569cbfcf9eef76ff

src/repositor{y,ies}: set ::load to the real number
... by counting the actual number of products in the repository,
instead of directly using the ::load field in the repository
document. (Directly inserting this field is also discouraged in
the future.)

src/repositories/ui.rs
@@ -1,6 +1,10 @@
 use serde::Serialize;
 use rocket_contrib::databases::mongodb;
-use rocket_contrib::databases::mongodb::db::ThreadedDatabase;
+use rocket_contrib::databases::mongodb::{
+  bson,
+  doc,
+  db::ThreadedDatabase,
+};
 use crate::database::UrmDb;
 use crate::repository::Repository;
 use crate::context::PageInfo;
@@ -31,6 +35,12 @@ impl<'a> Context<'a> {
       .take(nitem as usize)
       .filter_map(|r| r.ok()) // XXX: TODO: Error handling
       .map(|r| Repository::from(r))
+      .map(|mut r| {
+        r.load = db.collection("products")
+          .count(Some(doc!{ "in": &r.ln_p }), None)
+          .unwrap_or(0) as u64;
+        r
+      })
       .collect();
 
     Ok(Context {

src/repository/ui.rs
@@ -19,14 +19,23 @@ impl<'a> Context<'a> {
   pub fn from_db(db: &UrmDb, config: &'a UrmConfig, ln_p: String)
     -> Result<Option<Self>, mongodb::error::Error>
   {
-    match db.collection("repositories")
+    let ctx = match db.collection("repositories")
       .find_one(Some(doc!{ "ln_p": ln_p }), None)?
     {
-      Some(doc) => Ok(Some(Context {
+      Some(doc) => Some(Context {
         urm: &config,
         repository: Repository::from(doc)
-      })),
-      None => Ok(None)
-    }
+      }),
+      None => None
+    }.and_then(|mut ctx| {
+      // Override Repository::has with the real number
+      ctx.repository.load = db.collection("products")
+        .count(Some(doc!{ "in": &ctx.repository.ln_p }), None)
+        .unwrap_or(0) as u64;
+
+      Some(ctx)
+    });
+
+    Ok(ctx)
   }
 }

commit 33b5ce185c100b893faed5d80c6434ddd8b1ff9a

src/repositor{y,ies}: switch to a non-fp paradigm
de92f6b introduced functional ways to gather data from the db, but
many errors are ignored since closures are unable to throw errors
to their parent functions directly using the ? operator. Change
back to a traditional programming paradigm to make error handling
better.

src/repositories/ui.rs
@@ -29,19 +29,18 @@ impl<'a> Context<'a> {
       max: nprod / (nitem + 1) + 1
     };
 
-    let repositories = db.collection("repositories")
+    let mut repositories: Vec<Repository> = db.collection("repositories")
       .find(None, None)?
       .skip(nskip as usize)
       .take(nitem as usize)
       .filter_map(|r| r.ok()) // XXX: TODO: Error handling
       .map(|r| Repository::from(r))
-      .map(|mut r| {
-        r.load = db.collection("products")
-          .count(Some(doc!{ "in": &r.ln_p }), None)
-          .unwrap_or(0) as u64;
-        r
-      })
       .collect();
+
+    for mut r in &mut repositories {
+      r.load = db.collection("products")
+        .count(Some(doc!{ "in": &r.ln_p }), None)? as u64;
+    }
 
     Ok(Context {
       urm: &config,

src/repository/ui.rs
@@ -19,7 +19,7 @@ impl<'a> Context<'a> {
   pub fn from_db(db: &UrmDb, config: &'a UrmConfig, ln_p: String)
     -> Result<Option<Self>, mongodb::error::Error>
   {
-    let ctx = match db.collection("repositories")
+    let mut ctx = match db.collection("repositories")
       .find_one(Some(doc!{ "ln_p": ln_p }), None)?
     {
       Some(doc) => Some(Context {
@@ -27,14 +27,13 @@ impl<'a> Context<'a> {
         repository: Repository::from(doc)
       }),
       None => None
-    }.and_then(|mut ctx| {
-      // Override Repository::load with the real number
-      ctx.repository.load = db.collection("products")
-        .count(Some(doc!{ "in": &ctx.repository.ln_p }), None)
-        .unwrap_or(0) as u64;
+    };
 
-      Some(ctx)
-    });
+    // Override Repository::load with the real number
+    if let Some(ref mut ctx) = ctx {
+      ctx.repository.load = db.collection("products")
+        .count(Some(doc!{ "in": &ctx.repository.ln_p }), None)? as u64;
+    };
 
     Ok(ctx)
   }