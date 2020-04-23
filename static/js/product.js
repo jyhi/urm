36a10e8577128f64f9cc9dfce56ce4095719f73d
Junde Yhi <lmy441900@aosc.xyz>
2020-04-23 17:10:27 +0800

    product: impl amount change on UI

--- static/js/product.js
+++ static/js/product.js
@@ -1,3 +1,18 @@
+$("#modal-change-btn-submit").click(() => {
+  if (!$("#modal-change-input-amount")[0].validity.valid)
+    return
+
+  $.ajax({
+    method: "PATCH",
+    url: "",
+    contentType: "application/json",
+    data: JSON.stringify({amount: $("#modal-change-input-amount").val()})
+  }).done(() => {
+    $("#modal-change").modal("hide")
+    location.reload()
+  })
+})
+
 $("#modal-remove-btn-remove").click(() => {
   $.ajax({
     method: "DELETE",
