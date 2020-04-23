03380ad8e0066e7de2960a95cd2886379b6c376a
Junde Yhi <lmy441900@aosc.xyz>
2020-04-23 23:30:02 +0800

    scripts/release.sh: add binary stripping option

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,11 @@ set -eo pipefail
 
 NAME="urm-$(git describe --always)-$(date '+%Y%m%d')"
 
+if [ "$1" = "strip" ]; then
+  STRIP=1
+  shift
+fi
+
 cargo build --release
 pushd utils/genuser
 cargo build --release
@@ -11,7 +16,12 @@ popd
 
 mkdir "${NAME}"
 
-cp target/release/urm target/release/genuser "${NAME}"
+cp target/release/{urm,genuser} "${NAME}"
+if [ "${STRIP}" ]; then
+  strip --strip-all "${NAME}/urm"
+  strip --strip-all "${NAME}/genuser"
+fi
+
 cp urm.toml Rocket.toml README.md "${NAME}"
 cp -r static/ templates/ "${NAME}"
 
