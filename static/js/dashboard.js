2d64f9c3e6420a431dab8dc4c0e16c23ff597772
Junde Yhi <lmy441900@aosc.xyz>
2020-04-26 12:35:24 +0800

    dashboard: impl stock in

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -13,10 +13,10 @@ $("#modal-stockin-btn-add-field").click(() => {
 <div class="row no-gutters mt-3 field" id="modal-stockin-field${n + 1}">
   <div class="col">
     <label for="modal-stockin-input-field${n + 1}-key" class="sr-only">Key</label>
-    <input class="form-control" id="modal-stockin-input-field${n + 1}-key" placeholder="key" required>
+    <input class="form-control key" id="modal-stockin-input-field${n + 1}-key" placeholder="key" required>
     <div class="my-2"></div>
     <label for="modal-stockin-input-field${n + 1}-value" class="sr-only">Value</label>
-    <input class="form-control" id="modal-stockin-input-field${n + 1}-value" placeholder="value" required>
+    <input class="form-control value" id="modal-stockin-input-field${n + 1}-value" placeholder="value" required>
   </div>
   <div class="col-auto ml-2 d-flex align-items-stretch">
     <button class="btn btn-block btn-outline-danger" id="modal-stockin-btn-remove-field${n + 1}" aria-label="Remove this field" onclick="removeField('#modal-stockin-field${n + 1}')">&times;</button>
@@ -27,3 +27,26 @@ $("#modal-stockin-btn-add-field").click(() => {
 const removeField = (f) => {
   $(f).detach();
 }
+
+$("#modal-stockin-btn-submit").click(() => {
+  const validity = $
+    .map($("#modal-stockin-body input"), (v) => v.validity.valid)
+    .reduce((a, v) => a & v, true)
+  if (!validity)
+    return
+
+  const keys = $.map($("#modal-stockin-body .real-key"), (v) => v.textContent)
+    .concat($.map($("#modal-stockin-body .key"), (v) => v.value))
+  const values = $.map($("#modal-stockin-body .value"), (v) => v.value)
+  const object = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
+
+  $.ajax({
+    method: "POST",
+    url: "/product",
+    contentType: "application/json",
+    data: JSON.stringify(object)
+  }).done(() => {
+    $("#modal-stockin").modal("hide")
+    location.reload()
+  })
+})
