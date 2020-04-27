e733ae4d9f08ce66b220a842adb3be2669d89e3e
Junde Yhi <lmy441900@aosc.xyz>
2020-04-27 15:21:11 +0800

    repository: impl edit

--- static/js/repository.js
+++ static/js/repository.js
@@ -1,3 +1,25 @@
+$("#modal-edit-btn-submit").click(() => {
+  const validity = $
+    .map($("#modal-edit-body input"), (v) => v.validity.valid)
+    .reduce((a, v) => a & v, true)
+  if (!validity)
+    return
+
+  const keys = $.map($("#modal-edit-body label"), (v) => v.textContent)
+  const values = $.map($("#modal-edit-body input"), (v) => v.value)
+  const object = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
+
+  $.ajax({
+    method: "PUT",
+    url: "",
+    contentType: "application/json",
+    data: JSON.stringify(object)
+  }).done(() => {
+    $("#modal-edit").modal("hide")
+    location.reload()
+  })
+})
+
 $("#modal-remove-btn-remove").click(() => {
   $.ajax({
     method: "DELETE",
