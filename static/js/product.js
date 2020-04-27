3edf340a2c5b49fa2ee6ed7a0258f03fd34a2a5e
Junde Yhi <lmy441900@aosc.xyz>
2020-04-27 16:09:52 +0800

    product, repository: impl edit

--- static/js/product.js
+++ static/js/product.js
@@ -1,3 +1,25 @@
+$("#modal-edit-btn-add-field").click(() => {
+  const n = $("#modal-edit-body .field").length;
+
+  $("#modal-edit-body").append(`
+<div class="row no-gutters mt-3 field" id="modal-edit-field${n + 1}">
+  <div class="col">
+    <label for="modal-edit-input-field${n + 1}-key" class="sr-only">Key</label>
+    <input class="form-control key" id="modal-edit-input-field${n + 1}-key" placeholder="key" required>
+    <div class="my-2"></div>
+    <label for="modal-edit-input-field${n + 1}-value" class="sr-only">Value</label>
+    <input class="form-control value" id="modal-edit-input-field${n + 1}-value" placeholder="value" required>
+  </div>
+  <div class="col-auto ml-2 d-flex align-items-stretch">
+    <button class="btn btn-block btn-outline-danger" id="modal-edit-btn-remove-field${n + 1}" aria-label="Remove this field" onclick="removeField('#modal-edit-field${n + 1}')">&times;</button>
+  </div>
+</div>`)
+})
+
+const removeField = (f) => {
+  $(f).detach();
+}
+
 $("#modal-change-btn-submit").click(() => {
   if (!$("#modal-change-input-amount")[0].validity.valid)
     return
@@ -20,8 +42,9 @@ $("#modal-edit-btn-submit").click(() => {
   if (!validity)
     return
 
-  const keys = $.map($("#modal-edit-body label"), (v) => v.textContent)
-  const values = $.map($("#modal-edit-body input"), (v) => v.value)
+  const keys = $.map($("#modal-edit-body .real-key"), (v) => v.textContent)
+    .concat($.map($("#modal-edit-body .key"), (v) => v.value))
+  const values = $.map($("#modal-edit-body .value"), (v) => v.value)
   const object = Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
 
   $.ajax({
